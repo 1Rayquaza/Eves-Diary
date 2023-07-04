d700d086937f1bfa3dda70ddb4ce7ae8ec5b9eab
1Rayquaza <rishitabarnwal4096@gmail.com>
2023-07-05 01:52:35 +0530

    Improved Insights and Exercise fragment

--- app/src/main/java/com/example/evesdiary/ui/insights/InsightsFragment.java
+++ app/src/main/java/com/example/evesdiary/ui/insights/InsightsFragment.java
@@ -9,7 +9,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.evesdiary.ArticleAdapter;
+import com.example.evesdiary.ArticleData;
+import com.example.evesdiary.R;
 import com.example.evesdiary.databinding.FragmentInsightsBinding;
 
 public class InsightsFragment extends Fragment {
@@ -24,8 +30,22 @@ public class InsightsFragment extends Fragment {
         binding = FragmentInsightsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textInsights;
-        insightsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ArticleData[] articleData = new ArticleData[]{
+                new ArticleData("Menarche", "https://www.firstperiod.org/", R.drawable.menarche),
+                new ArticleData("Menopause", "https://www.reddit.com/r/Menopause/wiki/index/#wiki_r.2Fmenopause_wiki", R.drawable.menopause),
+                new ArticleData("Self Care", "https://bloodybrilliant.wales/my-period/period-self-care/", R.drawable.selfcare),
+                new ArticleData("Period Products", "https://kidshealth.org/en/teens/supplies.html", R.drawable.products),
+                new ArticleData("Health Disorders", "https://kidshealth.org/en/teens/supplies.html", R.drawable.health_disorders),
+                new ArticleData("Cycle Phases", "https://kidshealth.org/en/teens/supplies.html", R.drawable.phases),
+
+        };
+
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        ArticleAdapter articleAdapter = new ArticleAdapter(articleData);
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        recyclerView.setAdapter(articleAdapter);
         return root;
     }
 
